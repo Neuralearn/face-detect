d85c73762322fbb0392e9912bb38fd4214eaa0d7
root <martinsderick99@gmail.com>
2022-02-03 15:16:47 +0100

    first commit!

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ app.include_router(api_router, prefix=API)
 
 @app.get("/ping", summary="Check that the service is operational")
 def pong():
-
-    """
-    Sanity check - this will let the user know that the service is operational.
-    It is also used as part of the HEALTHCHECK. Docker uses curl to check that the API service is still running, by exercising this endpoint.
-    """
+    
     
     return {"ping": "pong!"}
